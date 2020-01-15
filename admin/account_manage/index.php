3d236cd1557b72e93a99f7f18f5102c271fd275e
nozoyama39 <nozoyama39@gmail.com>
2020-01-15 20:51:49 +0900

    Update index.php

--- admin/account_manage/index.php
+++ admin/account_manage/index.php
@@ -122,9 +122,10 @@ $script .= '$(document).ready(function() {
         order: [], // 初期表示時には並び替えをしない
         lengthMenu: [[ 25, 50, 100, -1 ],[25, 50, 100, "全件"]],
         columnDefs: [
-            { "orderable": false, "targets": 5 },
-            { "orderable": false, "targets": 7 },
-            { "orderable": true, "orderDataType": "part", "targets":1 }
+            { "orderable": false, "targets": 0 },
+            { "orderable": false, "targets": 6 },
+            { "orderable": false, "targets": 8 },
+            { "orderable": true, "orderDataType": "part", "targets": 2 }
         ],
         deferRender : false,
         autowidth: false,
