7f90045facfa2ca152480c552bcacd47919a4bbe
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-01-16 12:35:33 +0900

    Update index.php

--- admin/account_manage/index.php
+++ admin/account_manage/index.php
@@ -34,77 +34,79 @@ include_once('/home/chorkleines/www/member/mypage/Common/head.php');
                 echo '<form action="/member/mypage/admin/account_manage/change_admin.php" method="POST" id="form">';
             }
             ?>
-            <table id="accountList" class="table table-bordered table-striped mb-4" style="width: 100%;">
-                <thead>
-                    <tr>
-                        <?php
-                        if ($user->admin == 1) {
-                            echo '<th class="text-nowrap"></th>';
-                        }
-                        ?>
-                        <th class="text-nowrap">学年</th>
-                        <th class="text-nowrap">パート</th>
-                        <th class="text-nowrap">氏名</th>
-                        <?php
-                        if ($user->admin == 1) {
-                            echo '<th class="text-nowrap">滞納額</th>';
-                        }
-                        ?>
-                        <th class="text-nowrap">メールアドレス</th>
-                        <th class="text-nowrap">パスワード</th>
-                        <?php
-                        if ($user->admin == 1) {
-                            echo '<th class="text-nowrap">管理者権限</th>';
-                        }
-                        ?>
-                        <th class="text-nowrap">削除</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $query = "SELECT * FROM members ORDER BY grade ASC, CASE WHEN part LIKE 'S' THEN 1 WHEN part LIKE 'A' THEN 2 WHEN part LIKE 'T' THEN 3 WHEN part LIKE 'B' THEN 4 END ASC, kana ASC";
-                    $result = $mysqli->query($query);
-                    if (!$result) {
-                        print('Query Failed : ' . $mysqli->error);
-                        $mysqli->close();
-                        exit();
-                    }
-                    $row_cnt = $result->num_rows;
-                    while ($row = $result->fetch_assoc()) {
-                        $account = new User($row);
-                        echo '<tr>';
-                        if ($user->admin == 1) {
-                            echo '<td><div class="form-check form-check-inline"><input class="form-check-input" type="checkbox" name="check[]" value="' . $account->id . '"></div></td>';
-                        }
-                        echo '<td class="text-nowrap">' . $account->grade . '</td>';
-                        echo '<td class="text-nowrap">' . $account->get_part() . '</td>';
-                        echo '<td class="text-nowrap"><span class="d-none">' . $account->kana . '</span>' . $account->name . '</td>';
-                        if ($user->admin == 1) {
-                            $query = "SELECT * FROM fee_record_$account->id WHERE datetime IS NULL";
-                            $result_2 = $mysqli->query($query);
-                            if (!$result_2) {
-                                print('Query Failed : ' . $mysqli->error);
-                                $mysqli->close();
-                                exit();
+            <div class="mb-4">
+                <table id="accountList" class="table table-bordered table-striped" style="width: 100%;">
+                    <thead>
+                        <tr>
+                            <?php
+                            if ($user->admin == 1) {
+                                echo '<th class="text-nowrap"></th>';
                             }
-                            $delinquent = 0;
-                            while ($row_2 = $result_2->fetch_assoc()) {
-                                $fee = new Fee($row_2);
-                                $delinquent += $fee->price;
+                            ?>
+                            <th class="text-nowrap">学年</th>
+                            <th class="text-nowrap">パート</th>
+                            <th class="text-nowrap">氏名</th>
+                            <?php
+                            if ($user->admin == 1) {
+                                echo '<th class="text-nowrap">滞納額</th>';
                             }
-                            echo '<td class="text-nowrap">' . $delinquent . '</td>';
+                            ?>
+                            <th class="text-nowrap">メールアドレス</th>
+                            <th class="text-nowrap">パスワード</th>
+                            <?php
+                            if ($user->admin == 1) {
+                                echo '<th class="text-nowrap">管理者権限</th>';
+                            }
+                            ?>
+                            <th class="text-nowrap">削除</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $query = "SELECT * FROM members ORDER BY grade ASC, CASE WHEN part LIKE 'S' THEN 1 WHEN part LIKE 'A' THEN 2 WHEN part LIKE 'T' THEN 3 WHEN part LIKE 'B' THEN 4 END ASC, kana ASC";
+                        $result = $mysqli->query($query);
+                        if (!$result) {
+                            print('Query Failed : ' . $mysqli->error);
+                            $mysqli->close();
+                            exit();
                         }
-                        echo '<td class="text-nowrap">' . $account->email . '</td>';
-                        echo '<td class="text-nowrap">' . $account->get_password() . '</td>';
-                        if ($user->admin == 1) {
-                            echo '<td class="text-nowrap">' . $account->get_admin() . '</td>';
+                        $row_cnt = $result->num_rows;
+                        while ($row = $result->fetch_assoc()) {
+                            $account = new User($row);
+                            echo '<tr>';
+                            if ($user->admin == 1) {
+                                echo '<td><div class="form-check form-check-inline"><input class="form-check-input" type="checkbox" name="check[]" value="' . $account->id . '"></div></td>';
+                            }
+                            echo '<td class="text-nowrap">' . $account->grade . '</td>';
+                            echo '<td class="text-nowrap">' . $account->get_part() . '</td>';
+                            echo '<td class="text-nowrap"><span class="d-none">' . $account->kana . '</span>' . $account->name . '</td>';
+                            if ($user->admin == 1) {
+                                $query = "SELECT * FROM fee_record_$account->id WHERE datetime IS NULL";
+                                $result_2 = $mysqli->query($query);
+                                if (!$result_2) {
+                                    print('Query Failed : ' . $mysqli->error);
+                                    $mysqli->close();
+                                    exit();
+                                }
+                                $delinquent = 0;
+                                while ($row_2 = $result_2->fetch_assoc()) {
+                                    $fee = new Fee($row_2);
+                                    $delinquent += $fee->price;
+                                }
+                                echo '<td class="text-nowrap">' . $delinquent . '</td>';
+                            }
+                            echo '<td class="text-nowrap">' . $account->email . '</td>';
+                            echo '<td class="text-nowrap">' . $account->get_password() . '</td>';
+                            if ($user->admin == 1) {
+                                echo '<td class="text-nowrap">' . $account->get_admin() . '</td>';
+                            }
+                            echo '<td class="text-nowrap"><button type="submit" name="delete" formaction="/member/mypage/admin/account_manage/delete_user.php" class="btn btn-danger btn-sm" value="' . $account->id . '" Onclick="return confirm(\'' . $account->name . 'さんのアカウントを削除しますか？\nこれを実行すると会計記録も消えます！\');">削除</button></td>';
+                            echo '</tr>';
                         }
-                        echo '<td class="text-nowrap"><button type="submit" name="delete" formaction="/member/mypage/admin/account_manage/delete_user.php" class="btn btn-danger btn-sm" value="' . $account->id . '" Onclick="return confirm(\'' . $account->name . 'さんのアカウントを削除しますか？\nこれを実行すると会計記録も消えます！\');">削除</button></td>';
-                        echo '</tr>';
-                    }
-                    ?>
-                </tbody>
-            </table>
+                        ?>
+                    </tbody>
+                </table>
+            </div>
             <?php
             if ($user->admin == 1) {
                 echo '<span class="dropdown">
