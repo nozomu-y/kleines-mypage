743238e1518296ddba697e9f20a1e0282296f6fd
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-01-16 15:12:50 +0900

    Update User.php

--- Class/User.php
+++ Class/User.php
@@ -132,6 +132,6 @@ class Fee
 
     public function get_submission_time()
     {
-        return date('Y/m/d H:i:s', strtotime($this->deadline));
+        return date('Y/m/d H:i:s', strtotime($this->datetime));
     }
 }
