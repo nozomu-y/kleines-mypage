b1224df5d3c757b6ae9ea3000e8da4568c818129
nozoyama39 <nozoyama39@gmail.com>
2020-01-16 12:36:57 +0900

    Update index.php

--- admin/account_manage/index.php
+++ admin/account_manage/index.php
@@ -168,7 +168,7 @@ $script .= '],
         scrollX: true,
         // fixedHeader: true
          dom:"<\'row\'<\'col-sm-6\'l><\'col-sm-6 right\'f>>" +
-            "<\'row\'<\'col-sm-12\'tr>>" +
+            "<\'row\'<\'col-sm-12 mb-2\'tr>>" +
             "<\'row\'<\'col-sm-6\'i><\'col-sm-6\'p>>"
     }); 
 });';
