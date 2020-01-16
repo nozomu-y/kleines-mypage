fd205d6bb2b7fcf864e891ca646851e8d6701785
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-01-16 15:37:40 +0900

    Update index.php

--- accounting/individual/index.php
+++ accounting/individual/index.php
@@ -54,7 +54,7 @@ include_once('/home/chorkleines/www/member/mypage/Common/head.php');
                         <tr role="row" class="odd">
                             <td class="text-nowrap">2019/06/08</td>
                             <td class="text-nowrap">団費集金（1～6月分）</td>
-                            <td class="text-nowrap">￥-10600</td>
+                            <td class="text-nowrap">￥10600</td>
                         </tr>
                     </tbody>
                     <tfoot>
