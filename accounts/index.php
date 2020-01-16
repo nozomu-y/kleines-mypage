7e2fc2ed078d2036b51b206edc38bbfc6a34f455
nozoyama39 <nozoyama39@gmail.com>
2020-01-16 17:00:25 +0900

    Update index.php

--- accounts/index.php
+++ accounts/index.php
@@ -116,7 +116,7 @@ $script .= '$(document).ready(function() {
         // dom:"<\'row\'<\'col-sm-6\'l><\'col-sm-6 right\'f>>" +
         //     "<\'row\'<\'col-sm-12 mb-2\'tr>>" +
         //     "<\'row\'<\'col-sm-6\'i><\'col-sm-6\'p>>"
-    }); =
+    });
 });';
 $script .= '$.fn.dataTable.ext.order["part"] = function(settings, col) {
             return this.api().column(col, {
