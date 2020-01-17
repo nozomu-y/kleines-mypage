4226255624d0e0dbd1e24760b0f09cdc77b5969b
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-01-18 01:27:19 +0900

    update

---
+++ admin/accounting/change_status_paid.php
@@ -0,0 +1,25 @@
+<?php
+ob_start();
+session_start();
+if (!isset($_SESSION['mypage_email'])) {
+    header('Location: /member/mypage/login/');
+    exit();
+}
+
+require_once('/home/chorkleines/www/member/mypage/Core/dbconnect.php');
+$email = $_SESSION['mypage_email'];
+$query = "SELECT * FROM members WHERE email='$email'";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$user = new User($result->fetch_assoc());
+
+if (!($user->admin == 1 || $user->admin == 3)) {
+    header('Location: /member/mypage/');
+    exit();
+}
+
+$fee_id = $_GET['fee_id'];
