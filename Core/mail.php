e4a5bf9372371ffcfd6af0cafc50f22aac62ab68
nozoyama39 <nozoyama39@gmail.com>
2020-01-18 22:50:22 +0900

    paid unpaid

---
+++ Core/mail.php
@@ -0,0 +1,48 @@
+<?php
+require '/home/chorkleines/www/member/download/18/mypage/googleapi/vendor/autoload.php';
+
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Gmail API PHP Quickstart');
+    $client->setScopes(Google_Service_Gmail::GMAIL_READONLY);
+    $client->setScopes('https://www.googleapis.com/auth/gmail.send');
+    $client->setAuthConfig('/home/chorkleines/www/member/download/18/mypage/googleapi/credentials.json');
+    $client->setApprovalPrompt('auto');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    $tokenPath = '/home/chorkleines/www/member/download/18/mypage/googleapi/token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+}
--- admin/accounting/change_status_paid.php
+++ admin/accounting/change_status_paid.php
@@ -22,4 +22,102 @@ if (!($user->admin == 1 || $user->admin == 3)) {
     exit();
 }
 
-$fee_id = $_GET['fee_id'];
+if (!isset($_POST['fee_id'])) {
+    header('Location: /member/mypage/admin/accounting/');
+    exit();
+}
+
+$fee_id = $_POST['fee_id'];
+$query = "SELECT * FROM fee_list WHERE id='$fee_id'";
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$fee_list = new Fee_List($fee_id);
+if ($fee_list->admin != 3) {
+    header('Location: /member/mypage/admin/accounting/');
+    exit();
+}
+
+$user_id = $_POST['user_id'];
+$price = $_POST['price'];
+$paid_cash = $_POST['paid_cash'];
+
+$query = "SELECT * FROM members WHERE id='$user_id'";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$account = new User($result->fetch_assoc());
+
+// amount of money paid from individual accounting
+$paid_individual = '-' . strval(intval($price) - intval($paid_cash));
+$query = "UPDATE fee_record_$account->id SET datetime = now(), paid_cash = $paid_cash WHERE id = $fee_id";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+
+// if individual accounting was used
+if (intval($price) - intval($paid_cash) > 0) {
+    $query = "SELECT id FROM individual_accounting_$account->id ORDER BY id ASC";
+    $result = $mysqli->query($query);
+    if (!$result) {
+        print('Query Failed : ' . $mysqli->error);
+        $mysqli->close();
+        exit();
+    }
+    while ($row = $result->fetch_assoc()) {
+        $list_id = $row['id'];
+    }
+    $list_id = $list_id + 1;
+    $query = "INSERT INTO individual_accounting_$account->id (id, date, name, price) VALUES ('$list_id', now(), '$fee_list->name', $paid_individual)";
+    $result = $mysqli->query($query);
+    if (!$result) {
+        print('Query Failed : ' . $mysqli->error);
+        $mysqli->close();
+        exit();
+    }
+}
+
+
+/* GoogleApi を用いてメールを送信する */
+require_once('/home/chorkleines/www/member/mypage/Core/mail.php');
+$msg = new Google_Service_Gmail_Message();
+$data = "";
+$data .= "To: " . $email_u . "\n"; //送信先
+$data .= "Cc: \n"; //CC
+$from = "コール・クライネス会計";
+$data .= "From: " . mb_encode_mimeheader($from, 'utf-8') . " <kleines.webmaster@gmail.com>\n";
+$subject = $name . '完了のお知らせ';
+$data .= "Subject: " . mb_encode_mimeheader($subject, 'utf-8') . "\n";
+$data .= "\n"; //改行２回でヘッダー部分を区別
+$body = "コール・クライネス会計です。\n" . $name . "（￥" . $price . "）の集金が完了致しました。\n";
+if (intval($price) - intval($paid_cash) == 0) {
+    $body .= "現金で￥" . $paid_cash . "を徴収しました。\n";
+} else {
+    $body .= "現金で￥" . $paid_cash . "を徴収し、￥" . strval(intval($price) - intval($paid_cash)) . "を個別会計の残高から差し引いています。\n";
+}
+$body .= "お支払いただきありがとうございます。\n";
+$body .= "\n支払った覚えのない方は会計またはWeb管までご連絡ください。";
+$body .= "\n\nマイページへのアクセスは https://www.chorkleines.com/member/mypage/ から。";
+$data .= $body;
+$data = base64_encode($data); //base64エンコードする
+$data = strtr($data, '+/', '-_'); //サニタイジング
+$data = rtrim($data, '='); //最後の'='を除去
+$msg->setRaw($data); //データをセット
+//オブジェクト生成
+$service = new Google_Service_Gmail($client);
+//メッセージ作成
+$message = $msg;
+//メッセージ送信
+$message = $service->users_messages->send("me", $message);
+// 値を正にする
+$paid_individual *= -1;
+/** ログファイル作成の処理 **/
+error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . "が" . $account->name . "の「" . $fee_list->name . "」の提出状況を既納に変更し、現金で￥" . $paid_cash . "受け取り、個別会計から￥" . $paid_individual . "差し引きました。\n", 3, "/home/chorkleines/www/member/mypage/Core/accounting.log");
