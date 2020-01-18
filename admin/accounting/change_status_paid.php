4b463379ce29cb30cebe79c6478abb945a2c6609
nozoyama39 <nozoyama39@gmail.com>
2020-01-19 01:13:36 +0900

    Update change_status_paid.php

--- admin/accounting/change_status_paid.php
+++ admin/accounting/change_status_paid.php
@@ -34,7 +34,7 @@ if (!$result) {
     $mysqli->close();
     exit();
 }
-print($fee_id);
+print($result->num_rows);
 $fee_list = new Fee_List($result->fetch_assoc());
 if ($fee_list->admin != 3) {
     header('Location: /member/mypage/admin/accounting/');
