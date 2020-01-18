8af01cb9b1910bc5101911edac09804c3eddf31f
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-01-18 20:46:12 +0900

    Update resign_list.php

--- admin/account_manage/resign_list.php
+++ admin/account_manage/resign_list.php
@@ -35,72 +35,70 @@ include_once('/home/chorkleines/www/member/mypage/Common/head.php');
                     <li class="breadcrumb-item active" aria-current="page">退団者リスト</li>
                 </ol>
             </nav>
-            <form action="/member/mypage/admin/account_manage/change_admin.php" method="POST" id="form">
-                <div class="mb-4">
-                    <table id="accountList" class="table table-bordered table-striped" style="width: 100%;">
-                        <thead>
-                            <tr>
-                                <th class="text-nowrap">学年</th>
-                                <th class="text-nowrap">パート</th>
-                                <th class="text-nowrap">氏名</th>
-                                <?php
-                                if ($user->admin == 1 || $user->admin == 3) {
-                                    echo '<th class="text-nowrap">滞納額</th>';
-                                }
-                                ?>
-                                <th class="text-nowrap">メールアドレス</th>
-                                <th class="text-nowrap">パスワード</th>
-                                <th class="text-nowrap">編集</th>
-                                <?php
-                                if ($user->admin == 1) {
-                                    echo '<th class="text-nowrap">削除</th>';
-                                }
-                                ?>
-                            </tr>
-                        </thead>
-                        <tbody>
+            <div class="mb-4">
+                <table id="accountList" class="table table-bordered table-striped" style="width: 100%;">
+                    <thead>
+                        <tr>
+                            <th class="text-nowrap">学年</th>
+                            <th class="text-nowrap">パート</th>
+                            <th class="text-nowrap">氏名</th>
                             <?php
-                            $query = "SELECT * FROM members ORDER BY grade ASC, CASE WHEN part LIKE 'S' THEN 1 WHEN part LIKE 'A' THEN 2 WHEN part LIKE 'T' THEN 3 WHEN part LIKE 'B' THEN 4 END ASC, kana ASC";
-                            $result = $mysqli->query($query);
-                            if (!$result) {
-                                print('Query Failed : ' . $mysqli->error);
-                                $mysqli->close();
-                                exit();
+                            if ($user->admin == 1 || $user->admin == 3) {
+                                echo '<th class="text-nowrap">滞納額</th>';
                             }
-                            $row_cnt = $result->num_rows;
-                            while ($row = $result->fetch_assoc()) {
-                                $account = new User($row);
-                                if ($account->status != 2) {
-                                    continue;
-                                }
-                                if ($account->delinquent != 0) {
-                                    $table_danger = 'class="table-danger"';
-                                } else {
-                                    $table_danger = '';
-                                }
-                                echo '<tr ' . $table_danger . '>';
-                                echo '<td class="text-nowrap">' . $account->grade . '</td>';
-                                echo '<td class="text-nowrap">' . $account->get_part() . '</td>';
-                                echo '<td class="text-nowrap"><span class="d-none">' . $account->kana . '</span>' . $account->name . '</td>';
-                                if ($user->admin == 1 || $user->admin == 3) {
-                                    echo '<td class="text-nowrap text-right">' . $account->get_delinquent() . '</td>';
-                                }
-                                echo '<td class="text-nowrap">' . $account->email . '</td>';
-                                echo '<td class="text-nowrap">' . $account->get_password() . '</td>';
-                                echo '<td class="text-nowrap">
+                            ?>
+                            <th class="text-nowrap">メールアドレス</th>
+                            <th class="text-nowrap">パスワード</th>
+                            <th class="text-nowrap">編集</th>
+                            <?php
+                            if ($user->admin == 1) {
+                                echo '<th class="text-nowrap">削除</th>';
+                            }
+                            ?>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $query = "SELECT * FROM members ORDER BY grade ASC, CASE WHEN part LIKE 'S' THEN 1 WHEN part LIKE 'A' THEN 2 WHEN part LIKE 'T' THEN 3 WHEN part LIKE 'B' THEN 4 END ASC, kana ASC";
+                        $result = $mysqli->query($query);
+                        if (!$result) {
+                            print('Query Failed : ' . $mysqli->error);
+                            $mysqli->close();
+                            exit();
+                        }
+                        $row_cnt = $result->num_rows;
+                        while ($row = $result->fetch_assoc()) {
+                            $account = new User($row);
+                            if ($account->status != 2) {
+                                continue;
+                            }
+                            if ($account->delinquent != 0) {
+                                $table_danger = 'class="tabe-danger"';
+                            } else {
+                                $table_danger = '';
+                            }
+                            echo '<tr ' . $table_danger . '>';
+                            echo '<td class="text-nowrap">' . $account->grade . '</td>';
+                            echo '<td class="text-nowrap">' . $account->get_part() . '</td>';
+                            echo '<td class="text-nowrap"><span class="d-none">' . $account->kana . '</span>' . $account->name . '</td>';
+                            if ($user->admin == 1 || $user->admin == 3) {
+                                echo '<td class="text-nowrap text-right">' . $account->get_delinquent() . '</td>';
+                            }
+                            echo '<td class="text-nowrap">' . $account->email . '</td>';
+                            echo '<td class="text-nowrap">' . $account->get_password() . '</td>';
+                            echo '<td class="text-nowrap">
                                 <button type="submit" name="present" formaction="/member/mypage/admin/account_manage/change_status.php" class="btn btn-secondary btn-sm" value="' . $account->id . '" Onclick="return confirm(\'' . $account->name . 'さんのステータスを在団にしますか？\');">在団</button>
                                 <button type="submit" name="absent" formaction="/member/mypage/admin/account_manage/change_status.php" class="btn btn-secondary btn-sm" value="' . $account->id . '" Onclick="return confirm(\'' . $account->name . 'さんのステータスを休団にしますか？\');">休団</button>
                             </td>';
-                                if ($user->admin == 1) {
-                                    echo '<td class="text-nowrap"><button type="submit" name="delete" formaction="/member/mypage/admin/account_manage/delete_user.php" class="btn btn-danger btn-sm" value="' . $account->id . '" Onclick="return confirm(\'' . $account->name . 'さんのアカウントを削除しますか？\nこのアカウントに関連する会計データが全て削除されます。\');">削除</button></td>';
-                                }
-                                echo '</tr>';
+                            if ($user->admin == 1) {
+                                echo '<td class="text-nowrap"><button type="submit" name="delete" formaction="/member/mypage/admin/account_manage/delete_user.php" class="btn btn-danger btn-sm" value="' . $account->id . '" Onclick="return confirm(\'' . $account->name . 'さんのアカウントを削除しますか？\nこのアカウントに関連する会計データが全て削除されます。\');">削除</button></td>';
                             }
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
-            </form>
+                            echo '</tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
         <div class="col-xl-3 col-sm-12">
 
