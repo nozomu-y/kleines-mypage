adbe26b8a30b833fee9c8805075ddf51e1d0122d
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-01-19 17:24:21 +0900

    Update edit_fee_list.php

--- admin/accounting/add_fee_list/edit_fee_list.php
+++ admin/accounting/add_fee_list/edit_fee_list.php
@@ -44,5 +44,5 @@ $_SESSION['mypage_update_fee'] = '';
 
 // make log file
 error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . "が新規集金リスト「" . $name . "」を編集しました。（期限：" . $deadline . "）\n", 3, "/home/chorkleines/www/member/mypage/Core/accounting.log");
-header('Location: detail.php?fee_id=' . $fee_id);
+header('Location: /member/mypage/admin/accounting/detail.php?fee_id=' . $fee_id);
 exit();
