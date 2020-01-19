dfe4179ecd14d693a7553d463b51c8c17ee21a9f
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-01-19 18:04:24 +0900

    session

--- admin/account_manage/change_admin.php
+++ admin/account_manage/change_admin.php
@@ -40,6 +40,8 @@ if (isset($_POST['admin-give-1'])) {
             $mysqli->close();
             exit();
         }
+        $_SESSION['mypage_account_name'] = $account->get_name();
+        $_SESSION['mypage_admin'] = "マスター権限";
         // make log file
         error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . " gave the administrator right (master) to " . $account->name . ". \n", 3, "/home/chorkleines/www/member/mypage/Core/account_manage.log");
     }
@@ -63,6 +65,8 @@ if (isset($_POST['admin-give-1'])) {
             $mysqli->close();
             exit();
         }
+        $_SESSION['mypage_account_name'] = $account->get_name();
+        $_SESSION['mypage_admin'] = "アカウント管理";
         error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . " gave the administrator right (account management) to " . $account->name . ". \n", 3, "/home/chorkleines/www/member/mypage/Core/account_manage.log");
     }
     header('Location: /member/mypage/admin/account_manage/');
@@ -85,6 +89,8 @@ if (isset($_POST['admin-give-1'])) {
             $mysqli->close();
             exit();
         }
+        $_SESSION['mypage_account_name'] = $account->get_name();
+        $_SESSION['mypage_admin'] = "会計システム";
         error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . " gave the administrator right (accounting management) to " . $account->name . ". \n", 3, "/home/chorkleines/www/member/mypage/Core/account_manage.log");
     }
     header('Location: /member/mypage/admin/account_manage/');
@@ -107,6 +113,8 @@ if (isset($_POST['admin-give-1'])) {
             $mysqli->close();
             exit();
         }
+        $_SESSION['mypage_account_name'] = $account->get_name();
+        $_SESSION['mypage_admin'] = "練習計画管理";
         error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . " gave the administrator right (practice schedule management) to " . $account->name . ". \n", 3, "/home/chorkleines/www/member/mypage/Core/account_manage.log");
     }
     header('Location: /member/mypage/admin/account_manage/');
@@ -129,6 +137,8 @@ if (isset($_POST['admin-give-1'])) {
             $mysqli->close();
             exit();
         }
+        $_SESSION['mypage_account_name'] = $account->get_name();
+        $_SESSION['mypage_admin'] = "合宿会計システム";
         error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . " gave the administrator right (camp accounting management) to " . $account->name . ". \n", 3, "/home/chorkleines/www/member/mypage/Core/account_manage.log");
     }
     header('Location: /member/mypage/admin/account_manage/');
@@ -151,6 +161,8 @@ if (isset($_POST['admin-give-1'])) {
             $mysqli->close();
             exit();
         }
+        $_SESSION['mypage_account_name'] = $account->get_name();
+        $_SESSION['mypage_admin_deprive'] = "";
         /** ログファイル作成の処理 **/
         error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . " deprived " . $account->name . " of the administrator right (" . $account->get_admin_en() . "). \n", 3, "/home/chorkleines/www/member/mypage/Core/account_manage.log");
     }
