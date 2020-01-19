ba781b80b0957609e1d6e4a9d6b357b844878fae
nozoyama39 <nozoyama39@gmail.com>
2020-01-19 18:04:24 +0900

    session

--- admin/account_manage/change_status.php
+++ admin/account_manage/change_status.php
@@ -39,6 +39,8 @@ if (isset($_POST['present'])) {
         $mysqli->close();
         exit();
     }
+    $_SESSION['mypage_account_name'] = $account->get_name();
+    $_SESSION['mypage_status'] = "在団";
     // make log file
     error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . " changed the status of " . $account->name . " to present. \n", 3, "/home/chorkleines/www/member/mypage/Core/account_manage.log");
     header('Location: /member/mypage/admin/account_manage/');
@@ -60,6 +62,8 @@ if (isset($_POST['present'])) {
         $mysqli->close();
         exit();
     }
+    $_SESSION['mypage_account_name'] = $account->get_name();
+    $_SESSION['mypage_status'] = "休団";
     // make log file
     error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . " changed the status of " . $account->name . " to absent. \n", 3, "/home/chorkleines/www/member/mypage/Core/account_manage.log");
     header('Location: /member/mypage/admin/account_manage/');
@@ -91,6 +95,8 @@ if (isset($_POST['present'])) {
         }
         error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . " deprived " . $account->name . " of the administrator right (" . $account->get_admin_en() . "). \n", 3, "/home/chorkleines/www/member/mypage/Core/account_manage.log");
     }
+    $_SESSION['mypage_account_name'] = $account->get_name();
+    $_SESSION['mypage_status'] = "退団";
     // make log file
     error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . " changed the status of " . $account->name . " to resign. \n", 3, "/home/chorkleines/www/member/mypage/Core/account_manage.log");
     header('Location: /member/mypage/admin/account_manage/');
