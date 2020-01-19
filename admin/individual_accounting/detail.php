f110492dc9d8ed3a3be08726d9e70bb55644ef9e
nozoyama39 <nozoyama39@gmail.com>
2020-01-19 18:49:37 +0900

    uodate

---
+++ admin/individual_accounting/detail.php
@@ -0,0 +1,140 @@
+<?php
+ob_start();
+session_start();
+if (!isset($_SESSION['mypage_email'])) {
+    header('Location: /member/mypage/login/');
+    exit();
+}
+
+require_once('/home/chorkleines/www/member/mypage/Core/dbconnect.php');
+$email = $_SESSION['mypage_email'];
+$query = "SELECT * FROM members WHERE email='$email'";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$user = new User($result->fetch_assoc());
+
+if (!($user->admin == 1 || $user->admin == 3)) {
+    header('Location: /member/mypage/');
+    exit();
+}
+
+if (isset($_GET['account_id'])) {
+    $account_id = $_GET['account_id'];
+} else {
+    header('Location: /member/mypage/admin/accounting/');
+    exit();
+}
+
+$query = "SELECT * FROM members WHERE id=$account_id";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$account = new User($result->fetch_assoc());
+
+include_once('/home/chorkleines/www/member/mypage/Common/head.php');
+?>
+
+<div class="container-fluid">
+    <h1 class="h3 text-gray-800 mb-4">個別会計管理</h1>
+    <div class="row">
+        <div class=" col-xl-9 col-sm-12">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/member/mypage/admin/accounting/">個別会計管理</a></li>
+                    <li class="breadcrumb-item active" aria-current="page"><? echo $account->get_name(); ?></li>
+                </ol>
+            </nav>
+            <div class="mb-4">
+                <table id="accountingList" class="table table-bordered table-striped" style="width: 100%;">
+                    <thead>
+                        <tr>
+                            <th class="text-nowrap">日付</th>
+                            <th class="text-nowrap">項目</th>
+                            <th class="text-nowrap">金額</th>
+                            <th class="text-nowrap">編集</th>
+                            <th class="text-nowrap">削除</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $query = "SELECT * FROM individual_accounting_$account->id ORDER BY `date` ASC";
+                        $result = $mysqli->query($query);
+                        if (!$result) {
+                            print('Query Failed : ' . $mysqli->error);
+                            $mysqli->close();
+                            exit();
+                        }
+                        while ($row = $result->fetch_assoc()) {
+                            $individual = new Individual_Accounting($row);
+                            echo '<tr>
+                        <td class="text-nowrap">' . $individual->get_date() . '</td>
+                        <td class="text-nowrap">' . $individual->name . '</td>
+                        <td class="text-nowrap">' . $individual->get_price() . '</td>
+                        <td class="text-nowrap"><a href="edit.php?id=' . $account->id . '&list_id=' . $individual->id . '">編集</a></td>
+                        <td class="text-nowrap"><button type="submit" name="delete" class="btn btn-danger btn-sm" value="' . $individual->id . '" Onclick="return confirm(\'個別会計「' . $individual->name . '」を削除しますか？\');">削除</button></td>
+                        </tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+$script = '<script>';
+
+$script .= '$(document).ready(function() {
+    $("#accountingList").DataTable({
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Japanese.json"
+        },
+        order: [], // 初期表示時には並び替えをしない
+        lengthMenu: [[ 25, 50, 100, -1 ],[25, 50, 100, "全件"]],
+        columnDefs: [{ "orderable": false, "targets": 3 },
+            { "orderable": false, "targets": 4 },
+            { type: "currency", targets: 2 }],
+        deferRender : false,
+        autowidth: false,
+        scrollX: true,
+        // fixedHeader: true
+         dom:"<\'row\'<\'col-sm-6\'l><\'col-sm-6 right\'f>>" +
+            "<\'row\'<\'col-sm-12 mb-2\'tr>>" +
+            "<\'row\'<\'col-sm-6\'i><\'col-sm-6\'p>>"
+    }); 
+});';
+$script .= '$.fn.dataTable.ext.order["part"] = function(settings, col) {
+            return this.api().column(col, {
+                order: "index"
+            }).nodes().map(function(td, i) {
+                if (!$(td).html()) return 0;
+                if ($(td).html() == "Soprano") {
+                    return "b";
+                } else if ($(td).html() == "Alto") {
+                    return "c";
+                } else if ($(td).html() == "Tenor") {
+                    return "d";
+                } else if ($(td).html() == "Bass") {
+                    return "e";
+                } else {
+                    return "a";
+                }
+            });
+        }';
+$script .= '</script>';
+
+
+?>
+
+
+
+<?php
+include_once('/home/chorkleines/www/member/mypage/Common/foot.php');
