abd5314f033c9a7a6ebb7c20e1b0b26b21d7147e
nozoyama39 <nozoyama39@gmail.com>
2020-01-20 09:38:58 +0900

    Update delete_fee_list.php

--- admin/accounting/delete_fee_list.php
+++ admin/accounting/delete_fee_list.php
@@ -58,14 +58,24 @@ if (!$result) {
 }
 while ($row = $result->fetch_assoc()) {
     $account = new User($row);
-    $query = "DELETE FROM fee_record_$account->id WHERE id = $fee_id";
+    $query = "SELECT * FROM fee_record_$account->id WHERE id = $fee_id";
     $result_1 = $mysqli->query($query);
     if (!$result_1) {
         print('Query Failed : ' . $mysqli->error);
         $mysqli->close();
         exit();
     }
-    $query = "DELETE FROM individual_accounting_$account->id WHERE name = $fee_list->name";
+    $fee = new Fee($result_1->fetch_assoc());
+    if ($fee->paid_individual != 0) {
+        $query = "DELETE FROM individual_accounting_$account->id WHERE name = $fee_list->name";
+        $result_1 = $mysqli->query($query);
+        if (!$result_1) {
+            print('Query Failed : ' . $mysqli->error);
+            $mysqli->close();
+            exit();
+        }
+    }
+    $query = "DELETE FROM fee_record_$account->id WHERE id = $fee_id";
     $result_1 = $mysqli->query($query);
     if (!$result_1) {
         print('Query Failed : ' . $mysqli->error);
