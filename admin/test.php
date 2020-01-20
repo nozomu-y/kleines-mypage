874dad3c64b722097872aec9009e91e94c2a2ecb
nozoyama39 <nozoyama39@gmail.com>
2020-01-20 10:46:02 +0900

    Create test.php

---
+++ admin/test.php
@@ -0,0 +1,43 @@
+<?php
+ob_start();
+session_start();
+if (!isset($_SESSION['mypage_email'])) {
+    header('Location: /member/mypage/login/');
+    exit();
+}
+
+require_once('/home/chorkleines/www/member/mypage/Core/dbconnect.php');
+$email = $_SESSION['mypage_email'];
+$query = "SELECT * FROM members WHERE email='$email'";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$user = new User($result->fetch_assoc());
+
+if (!($user->admin == 1)) {
+    header('Location: /member/mypage/');
+    exit();
+}
+
+$query = "SELECT * FROM members ORDER BY id ASC";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+
+while ($row = $result->fetch_assoc()) {
+    $account = new User($row);
+    $query = "ALTER TABLE fee_record_$account->id DROP fee_id";
+    $result_1 = $mysqli->query($query);
+    if (!$result_1) {
+        print('Query Failed : ' . $mysqli->error);
+        $mysqli->close();
+        exit();
+    }
+    echo $account->id . ' ' . $account->get_name() . '<br>';
+}
