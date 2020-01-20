7bcc0554376b7605f0a7a34e1f5b87eee4dbc028
nozoyama39 <nozoyama39@gmail.com>
2020-01-20 21:22:31 +0900

    add individual

---
+++ admin/individual_accounting/add.php
@@ -0,0 +1,82 @@
+<?php
+ob_start();
+session_start();
+if (!isset($_SESSION['mypage_email'])) {
+    header('Location: /member/mypage/login/');
+    exit();
+}
+
+require_once('/home/chorkleines/www/member/mypage/Core/dbconnect.php');
+$email = $_SESSION['mypage_email'];
+$query = "SELECT * FROM members WHERE email='$email'";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$user = new User($result->fetch_assoc());
+
+if (!($user->admin == 1 || $user->admin == 3)) {
+    header('Location: /member/mypage/');
+    exit();
+}
+
+if (isset($_GET['account_id'])) {
+    $account_id = $_GET['account_id'];
+} else {
+    header('Location: /member/mypage/admin/accounting/');
+    exit();
+}
+
+$query = "SELECT * FROM members WHERE id=$account_id";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$account = new User($result->fetch_assoc());
+
+include_once('/home/chorkleines/www/member/mypage/Common/head.php');
+?>
+
+<div class="container-fluid">
+    <h1 class="h3 text-gray-800 mb-4">個別会計管理</h1>
+    <div class="row">
+        <div class=" col-xl-9 col-sm-12">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/member/mypage/admin/individual_accounting/">個別会計管理</a></li>
+                    <li class="breadcrumb-item"><a href="/member/mypage/admin/individual_accounting/detail.php?account_id=<?php echo $account->id; ?>"><? echo $account->get_name(); ?></a></li>
+                    <li class="breadcrumb-item active" aria-current="page">項目の追加</li>
+                </ol>
+            </nav>
+            <div class="mb-4">
+                <form action="edit_individual.php" method="post">
+                    <div class="form-group">
+                        <label for="date">日付</label>
+                        <input type="date" name="date" class="form-control" id="date" value="<?php echo date('Y-m-d'); ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">項目名</label>
+                        <input type="text" class="form-control" name="name" id="name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="price">金額</label>
+                        <input type="number" name="price" id="price" class="form-control" required>
+                    </div>
+                    <input type="hidden" name="account_id" value="<?php echo $account->id; ?>">
+                    <button type="submit" class="btn btn-primary" name="submit">追加する</button>
+                    <a class="btn btn-secondary" href="./detail.php?account_id=<?php echo $account->id; ?>" role="button">キャンセル</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php
+include_once('/home/chorkleines/www/member/mypage/Common/foot.php');
---
+++ admin/individual_accounting/add_individual.php
@@ -0,0 +1,75 @@
+<?php
+ob_start();
+session_start();
+if (!isset($_SESSION['mypage_email'])) {
+    header('Location: /member/mypage/login/');
+    exit();
+}
+
+require_once('/home/chorkleines/www/member/mypage/Core/dbconnect.php');
+$email = $_SESSION['mypage_email'];
+$query = "SELECT * FROM members WHERE email='$email'";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$user = new User($result->fetch_assoc());
+
+if (!($user->admin == 1 || $user->admin == 3)) {
+    header('Location: /member/mypage/');
+    exit();
+}
+
+if (!isset($_POST['submit'])) {
+    header('Location: /member/mypage/admin/individual_accounting/');
+    exit();
+}
+
+if (isset($_POST['account_id'])) {
+    $account_id = $_POST['account_id'];
+} else {
+    header('Location: /member/mypage/admin/accounting/');
+    exit();
+}
+
+$query = "SELECT * FROM members WHERE id=$account_id";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$account = new User($result->fetch_assoc());
+
+$date = $_POST['date'];
+$name = $_POST['name'];
+$price = $_POST['price'];
+
+$query = "SELECT id FROM individual_accounting_$account->id ORDER BY id ASC";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+while ($row = $result->fetch_assoc()) {
+    $list_id = $row['id'];
+}
+$list_id = $list_id + 1;
+
+$query = "INSERT INTO individual_accounting_$account->id (id, date, name, price) VALUES ('$list_id', '$date', '$name', '$price')";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query Failed : ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+
+
+$_SESSION['mypage_individual_add'] = $name;
+
+error_log("[" . date('Y/m/d H:i:s') . "] " . $user->name . "が" . $account->name . "の個別会計を追加しました。（項目名：" . $name . "　日付：" . $date . "　金額：" . $price . "）\n", 3, "/home/chorkleines/www/member/mypage/Core/individual_accounting.log");
+header('Location: detail.php?account_id=' . $account->id);
+exit();
