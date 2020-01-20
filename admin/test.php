25e0cc56d7d6828cd84a441a505c99cd8152e282
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-01-20 10:38:15 +0900

    Update test.php

--- admin/test.php
+++ admin/test.php
@@ -22,7 +22,8 @@ if (!($user->admin == 1)) {
     exit();
 }
 
-$query = "SELECT * FROM members ORDER BY id ASC";
+// $query = "SELECT * FROM members ORDER BY id ASC";
+$query = "SELECT * FROM members WHERE email='$email'";
 $result = $mysqli->query($query);
 if (!$result) {
     print('Query Failed : ' . $mysqli->error);
