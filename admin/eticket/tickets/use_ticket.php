a402e32d99877dc7bb1122c049e361270f8ea496
nozoyama39 <nozoyama39@gmail.com>
2020-03-01 17:37:04 +0900

    Update use_ticket.php

--- admin/eticket/tickets/use_ticket.php
+++ admin/eticket/tickets/use_ticket.php
@@ -27,33 +27,8 @@ if (!isset($_POST['data'])) {
     exit();
 }
 $data = $_POST['data'];
-$data = explode("redirect.php?list_id=", $data)[1];
 
-$list_id = substr($data, 0, 5);
-$ticket_id = substr($data, 5, 6);
-$token = substr($data, 11);
 
-$query = "SELECT * FROM ticket_list WHERE list_id = '$list_id'";
-$result = $mysqli->query($query);
-if (!$result) {
-    print('Query Failed : ' . $mysqli->error);
-    $mysqli->close();
-    exit();
-}
-while ($row = $result->fetch_assoc()) {
-    $ticket_list = new Ticket_List($row);
-}
-
-$query = "SELECT * FROM ticket_$list_id WHERE id = $ticket_id";
-$result = $mysqli->query($query);
-if (!$result) {
-    print('Query Failed : ' . $mysqli->error);
-    $mysqli->close();
-    exit();
-}
-while ($row = $result->fetch_assoc()) {
-    $ticket = new Ticket($row);
-}
 
 include_once('/home/chorkleines/www/member/mypage/Common/head.php');
 ?>
@@ -63,6 +38,39 @@ include_once('/home/chorkleines/www/member/mypage/Common/head.php');
     <div class="row">
         <div class=" col-xl-9 col-sm-12">
             <?php
+            if (strpos($data, "https://www.chorkleines.com/member/mypage/admin/eticket/redirect.php?list_id=") === false) {
+                echo "<p>QRコードが不正です</p>";
+                exit();
+            }
+
+            $data = explode("redirect.php?list_id=", $data)[1];
+
+            $list_id = substr($data, 0, 5);
+            $ticket_id = substr($data, 5, 6);
+            $token = substr($data, 11);
+
+            $query = "SELECT * FROM ticket_list WHERE list_id = '$list_id'";
+            $result = $mysqli->query($query);
+            if (!$result) {
+                print('Query Failed : ' . $mysqli->error);
+                $mysqli->close();
+                exit();
+            }
+            while ($row = $result->fetch_assoc()) {
+                $ticket_list = new Ticket_List($row);
+            }
+
+            $query = "SELECT * FROM ticket_$list_id WHERE id = $ticket_id";
+            $result = $mysqli->query($query);
+            if (!$result) {
+                print('Query Failed : ' . $mysqli->error);
+                $mysqli->close();
+                exit();
+            }
+            while ($row = $result->fetch_assoc()) {
+                $ticket = new Ticket($row);
+            }
+
             if ($token != $ticket->token) {
                 echo "<p>チケットが不正です</p>";
                 exit();
