e83dde2e36d64554d3f4e87bb737dfd7d24b0a4b
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-03-04 03:14:21 +0900

    Update head.php

--- Common/head.php
+++ Common/head.php
@@ -202,7 +202,7 @@ if ($backtrace == '/index.php') {
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <!-- <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $user->get_name(); ?></span> -->
-                                <!-- <span class="mr-2 text-gray-600 small"><?php echo $user->get_name(); ?></span> -->
+                                <span class="mr-2 text-gray-600 small"><?php echo $user->get_name(); ?></span>
                                 <i class="fas fa-user fa-fw"></i>
                             </a>
                             <!-- Dropdown - User Information -->
