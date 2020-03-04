e50913b17fd791703dd62be59bb7df4537a25cd1
nozoyama39 <nozoyama39@gmail.com>
2020-03-04 14:12:24 +0900

    Update sessions.php

--- profile/sessions.php
+++ profile/sessions.php
@@ -25,7 +25,7 @@ include_once('/home/chorkleines/www/member/mypage/Common/head.php');
     <h1 class="h3 text-gray-800">セッション管理</h1>
     <div class="row">
         <div class="col-sm-12">
-            <div class="mb-4">
+            <div class="mb-4 mt-3">
                 <form method="POST">
                     <table id="sessionList" class="table table-bordered table-striped" style="width: 100%;">
                         <thead>
@@ -68,7 +68,28 @@ include_once('/home/chorkleines/www/member/mypage/Common/head.php');
     </div>
 </div>
 
-
-
 <?php
+$script = '<script>';
+$script .= '$(document).ready(function() {
+    $("#accountList").DataTable({
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Japanese.json"
+        },
+        order: [], // 初期表示時には並び替えをしない
+        lengthMenu: [[ 25, 50, 100, -1 ],[25, 50, 100, "全件"]],
+        columnDefs: [';
+$script .= '{ "orderable": false, "targets": 3 }';
+$script .= '],
+        deferRender : false,
+        autowidth: false,
+        scrollX: true,
+        // fixedHeader: true
+         dom:"<\'row\'<\'col-sm-6\'l><\'col-sm-6 right\'f>>" +
+            "<\'row\'<\'col-sm-12 mb-2\'tr>>" +
+            "<\'row\'<\'col-sm-6\'i><\'col-sm-6\'p>>"
+    }); 
+});';
+$script .= '</script>';
+
+
 include_once('/home/chorkleines/www/member/mypage/Common/foot.php');
