df475ea85923ac30a50b5a8750f27aff6d8f5d40
nozoyama39 <nozoyama39@gmail.com>
2020-03-04 14:09:37 +0900

    Update sessions.php

--- profile/sessions.php
+++ profile/sessions.php
@@ -26,39 +26,41 @@ include_once('/home/chorkleines/www/member/mypage/Common/head.php');
     <div class="row">
         <div class="col-sm-12">
             <div class="mb-4">
-                <table id="sessionList" class="table table-bordered table-striped" style="width: 100%;">
-                    <thead>
-                        <tr>
-                            <th class="text-nowrap">ログイン日時</th>
-                            <th class="text-nowrap">プラットフォーム</th>
-                            <th class="text-nowrap">ブラウザ</th>
-                            <th class="text-nowrap"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $query = "SELECT * FROM auto_login WHERE id = '$user->id' ORDER BY datetime DESC";
-                        $result = $mysqli->query($query);
-                        if (!$result) {
-                            print('Query Failed : ' . $mysqli->error);
-                            $mysqli->close();
-                            exit();
-                        }
-                        while ($row = $result->fetch_assoc()) {
-                            $login_datetime = $row['datetime'];
-                            $login_platform = $row['device'];
-                            $login_browser = $row['browser'];
-                            $login_token = $row['token'];
-                            echo '<tr>';
-                            echo '<td class="text-nowrap">' . $login_datetime . '</td>';
-                            echo '<td class="text-nowrap">' . $login_platform . '</td>';
-                            echo '<td class="text-nowrap">' . $login_browser . '</td>';
-                            echo '<td class="text-nowrap"><button type="submit" name="delete" class="btn btn-outline-white btn-rounded btn-sm px-2" formaction="./delete_session.php" value="' . $login_token . '"><i class="far fa-trash-alt mt-0"></i></button></td>';
-                            echo '</tr>';
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                <form method="POST">
+                    <table id="sessionList" class="table table-bordered table-striped" style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th class="text-nowrap">ログイン日時</th>
+                                <th class="text-nowrap">プラットフォーム</th>
+                                <th class="text-nowrap">ブラウザ</th>
+                                <th class="text-nowrap"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $query = "SELECT * FROM auto_login WHERE id = '$user->id' ORDER BY datetime DESC";
+                            $result = $mysqli->query($query);
+                            if (!$result) {
+                                print('Query Failed : ' . $mysqli->error);
+                                $mysqli->close();
+                                exit();
+                            }
+                            while ($row = $result->fetch_assoc()) {
+                                $login_datetime = $row['datetime'];
+                                $login_platform = $row['device'];
+                                $login_browser = $row['browser'];
+                                $login_token = $row['token'];
+                                echo '<tr>';
+                                echo '<td class="text-nowrap">' . $login_datetime . '</td>';
+                                echo '<td class="text-nowrap">' . $login_platform . '</td>';
+                                echo '<td class="text-nowrap">' . $login_browser . '</td>';
+                                echo '<td class="text-nowrap"><button type="submit" name="delete" class="btn btn-danger rounded-circle btn-sm" formaction="./delete_session.php" value="' . $login_token . '"><i class="far fa-trash-alt mt-0"></i></button></td>';
+                                echo '</tr>';
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </form>
             </div>
         </div>
         <div class="col-xl-3 col-sm-12">
