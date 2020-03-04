e50279f0339c5127764b6d96e766b9f86a5cad7e
nozoyama39 <nozoyama39@gmail.com>
2020-03-04 12:45:58 +0900

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 session_start();
 
 // logout.phpにアクセスしたユーザーをログアウトする
-if (isset($_POST['logout'])) {
+if (isset($_GET['logout'])) {
     $_SESSION = array();
     setcookie(session_name(), '', time() - 1, '/');
     session_destroy();
