7587b90ad885a6f2dfcd6b62cbb22c73ef00d871
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-03-04 12:33:59 +0900

    added new function remember me

--- login/auth.php
+++ login/auth.php
@@ -9,7 +9,7 @@ require_once('/home/chorkleines/www/member/mypage/Core/dbconnect.php');
 
 if (isset($_POST['login'])) {
     $email = $mysqli->real_escape_string($_POST['email']);
-    $password = $mysqli->real_escape_string($_POST['password']);
+    $password = $_POST['password'];
     // start query
     $query = "SELECT * FROM members WHERE email='$email' AND status != 2";
     $result = $mysqli->query($query);
@@ -44,12 +44,67 @@ if (isset($_POST['login'])) {
         exit();
     } else {
         if (password_verify($password, $user->password)) {
+            // passed the authentication
             $query = "UPDATE members SET login_failure = 0 WHERE email='$user->email'";
             $result = $mysqli->query($query);
             if (!$result) {
                 print("Query Failed : " . $mysqli->error);
                 $mysqli->close();
                 exit();
+            }
+            // when "remember me" was selected at /login/index.php
+            if (isset($_GET['remember_me']) && $_GET['remember_me'] == "checked") {
+                // generate token
+                $token = sha1(uniqid(rand(), true) . mt_rand(1, 999999999) . '_mypage_auto_login');
+                // expiration time
+                $expiration_time = 3600 * 24 * 30; // token valid for 30 days
+                // set cookie
+                setcookie("mypage_auto_login", $token, time() + $expiration_time, "/member/mypage/", "chorkleines.com", false, true);
+                // check device(platform)
+                $user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+                if (preg_match('/ipod/i', $user_agent)) {
+                    $device = 'iPod';
+                } elseif (preg_match('/iphone/i', $user_agent)) {
+                    $device = 'iPhone';
+                } elseif (preg_match('/ipad/i', $user_agent)) {
+                    $device = 'iPad';
+                } elseif (preg_match('/android/i', $user_agent)) {
+                    $device = 'Android';
+                } elseif (preg_match('/windows phone/i', $user_agent)) {
+                    $device = 'Windows Phone';
+                } elseif (preg_match('/linux/i', $user_agent)) {
+                    $device = 'Linux';
+                } elseif (preg_match('/macintosh|mac os/i', $user_agent)) {
+                    $device = 'Mac';
+                } elseif (preg_match('/windows/i', $user_agent)) {
+                    $device = 'Windows';
+                } else {
+                    $device = '不明';
+                }
+                // check browser
+                if (strstr($user_agent, 'edge')) {
+                    $browser = 'Edge';
+                } elseif (strstr($user_agent, 'trident') || strstr($user_agent, 'msie')) {
+                    $browser = 'Internet Explorer';
+                } elseif (strstr($user_agent, 'chrome')) {
+                    $browser = 'Google Chrome';
+                } elseif (strstr($user_agent, 'firefox')) {
+                    $browser = 'Firefox';
+                } elseif (strstr($user_agent, 'safari')) {
+                    $browser = 'Safari';
+                } elseif (strstr($user_agent, 'opera')) {
+                    $browser = 'Opera';
+                } else {
+                    $browser = '不明';
+                }
+                // add to database
+                $query = "INSERT INTO auto_login (id, token, datetime, device, browser) VALUES ($user->id, $token, now(), $device, $browser)";
+                $result = $mysqli->query($query);
+                if (!$result) {
+                    print("Query Failed : " . $mysqli->error);
+                    $mysqli->close();
+                    exit();
+                }
             }
             $mysqli->close();
             // start session
--- logout.php
+++ logout.php
@@ -6,6 +6,21 @@ if (isset($_POST['logout'])) {
     $_SESSION = array();
     setcookie(session_name(), '', time() - 1, '/');
     session_destroy();
+
+    if (!empty($_COOKIE['mypage_auto_login'])) {
+        $token = $_COOKIE['mypage_auto_login'];
+        // delete token from database
+        $query = "DELETE FROM auto_login WHERE token = $token";
+        $result = $mysqli->query($query);
+        if (!$result) {
+            print("Query Failed : " . $mysqli->error);
+            $mysqli->close();
+            exit();
+        }
+        // delete token from browser cookie
+        setcookie("mypage_auto_login", "", time() - 60);
+    }
+
     header("Location: /member/mypage/login/");
     exit();
 } else {
