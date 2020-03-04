62dca75e03a87ae41f4713277f3d5e1b42a6580b
nozoyama39 <nozoyama39@gmail.com>
2020-03-04 13:16:08 +0900

    finish debug

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// logout.phpにアクセスしたユーザーをログアウトする
-if (isset($_GET['logout'])) {
-    $_SESSION = array();
-    setcookie(session_name(), '', time() - 1, '/');
-    session_destroy();
-}
