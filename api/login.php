2c0de14e26d6c91e9c8c2bbe32569a0ab41ec6d0
nozoyama39 <nozoyama39@gmail.com>
2020-04-18 15:40:00 +0900

    api

---
+++ api/login.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['email'])) {
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $password = $_POST['password'];
+    $query = "SELECT * FROM members WHERE email='$email' AND status != 2";
+    $result = $mysqli->query($query);
+    if (!$result) {
+        print("Query Failed : " . $mysqli->error);
+        $mysqli->close();
+        exit();
+    }
+    $user = new User($result->fetch_assoc());
+    $result->close();
+    if (password_verify($password, $user->password)) {
+        echo 'OK';
+    }
+}
