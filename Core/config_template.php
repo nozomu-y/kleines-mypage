2fcb3b50aeb4b416406fd55c9ec476259a3bd165
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-11-05 20:35:21 +0900

    Create config_template.php

---
+++ Core/config_template.php
@@ -0,0 +1,9 @@
+<?php
+define("DB_HOST", "");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
+
+define("MYPAGE_ROOT", "");
+define("WEB_DOMAIN", "");
+define("ADMIN_EMAIL", "");
