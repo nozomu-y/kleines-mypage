1f8f6763f070739989bb0742962e01c1ee7c4fae
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2020-11-09 13:35:04 +0900

    Bugfix: gmail api

--- googleapi/mail.php
+++ googleapi/mail.php
@@ -23,20 +23,9 @@ function getClient()
         if ($client->getRefreshToken()) {
             $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
         } else {
-            // Request authorization from the user.
-            $authUrl = $client->createAuthUrl();
-            printf("Open the following link in your browser:\n%s\n", $authUrl);
-            print 'Enter verification code: ';
-            $authCode = trim(fgets(STDIN));
-
-            // Exchange authorization code for an access token.
-            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-            $client->setAccessToken($accessToken);
-
-            // Check to see if there was an error.
-            if (array_key_exists('error', $accessToken)) {
-                throw new Exception(join(', ', $accessToken));
-            }
+            print("Please fetch a new token.\n");
+            print("Please contact the webmaster for further details.");
+            exit();
         }
         // Save the token to a file.
         if (!file_exists(dirname($tokenPath))) {
