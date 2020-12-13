507695d940487f24734e248882290b312dfb95f6
nozoyama39 <nozoyama39@gmail.com>
2020-12-13 19:21:33 +0900

    バグの修正

--- TicketSystems/index.php
+++ TicketSystems/index.php
@@ -1,4 +1,5 @@
 <?php
   //commit test
-  include_once(__DIR__."/view/index.php");
+  //include_once(__DIR__."/view/index.php");
+  header("Location: //".$_SERVER['SERVER_NAME']."/TicketSystems/mypage/TicketSystems/view/index.php");
 ?>
