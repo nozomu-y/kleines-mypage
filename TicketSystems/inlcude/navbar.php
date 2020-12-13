dc955a7e4f35e6e8dc80c031fb3982d703f70955
nozoyama39 <nozoyama39@gmail.com>
2020-12-13 19:41:06 +0900

    update navbar

--- TicketSystems/inlcude/navbar.php
+++ TicketSystems/inlcude/navbar.php
@@ -9,8 +9,23 @@
         <span id="cross1"></span><span id="cross2"></span>
       </label>
     </div>
-    <div id="nav-main">
-      <?php $return=include(ROOT."/include/nav-block.php");?>
+    <div id="nav-menu">
+      <div class="menu-block">
+        <input type="checkbox" id="menu-bar01" class="menu-bar">
+        <label for="menu-bar01">Menu title 01</label>
+        <div class="menu-item"><p><a href="">menu 1-1</a></p></div>
+        <div class="menu-item"><p><a href="">menu 1-2</a></p></div>
+        <div class="menu-item"><p><a href="">menu 1-3</a></p></div>
+        <div class="menu-item"><p><a href="">menu 1-4</a></p></div>
+      </div>
+      <div class="menu-block">
+        <input type="checkbox" id="menu-bar02" class="menu-bar">
+        <label for="menu-bar02">Menu title 02</label>
+        <div class="menu-item"><p><a href="">menu 2-1</a></p></div>
+        <div class="menu-item"><p><a href="">menu 2-2</a></p></div>
+        <div class="menu-item"><p><a href="">menu 2-3</a></p></div>
+        <div class="menu-item"><p><a href="">menu 2-4</a></p></div>
+      </div>
     </div>
   </div>
 </div>
