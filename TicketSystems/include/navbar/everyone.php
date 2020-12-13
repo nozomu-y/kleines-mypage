418f1f3893a30619223e43ed0cf848dc1f5801e2
yakou1000 <yakou10004129@gmail.com>
2020-12-14 00:03:30 +0900

    create each navbar

--- TicketSystems/include/navbar/everyone.php
+++ TicketSystems/include/navbar/everyone.php
@@ -1,9 +1,13 @@
 <div class="menu-block">
   <input type="checkbox" id="menu-bar01" class="menu-bar">
   <label for="menu-bar01">団員用メニュー</label>
-  <div class="menu-item"><p><a href="">menu 2-1</a></p></div>
-  <div class="menu-item"><p><a href="">menu 2-2</a></p></div>
-  <div class="menu-item"><p><a href="">menu 2-3</a></p></div>
-  <div class="menu-item"><p><a href="">menu 2-4</a></p></div>
+  <div class="menu-item"><p class="nav-link">チケットが欲しいとき</p><a href=""></a></div>
+  <div class="menu-item"><p class="nav-link">チケットを売ったとき</p><a href=""></a></div>
+  <div class="menu-item"><p class="nav-link">チケットを渉外に返したいとき</p><a href=""></a></div>
+  <div class="menu-item"><p class="nav-link">チケットがお客様から返品されたとき</p><a href=""></a></div>
+  <div class="menu-item"><p class="nav-link">情宣のアポが取れたとき</p><a href=""></a></div>
+  <div class="menu-item"><p class="nav-link">情宣一覧確認</p><a href=""></a></div>
+  <div class="menu-item"><p class="nav-link">配布済座席確認(指定席限定)</p><a href=""></a></div>
+  <div class="menu-item"><p class="nav-link">自分のチケット状況・履歴確認</p><a href=""></a></div>
 </div>
-<div class="menu-item"><p><a href="<?=SERVER."/views/private/index.php"?>">渉外用ページへ</a></p></div>
+<div class="menu-item"><p class="nav-link">渉外用ページへ</p><a href="<?=SERVER."/views/private/index.php"?>"></a></div>
--- TicketSystems/include/navbar/secret.php
+++ TicketSystems/include/navbar/secret.php
@@ -1,10 +1,9 @@
 <div class="menu-block">
   <input type="checkbox" id="menu-bar01" class="menu-bar">
   <label for="menu-bar01">渉外チーフ用メニュー</label>
-  <div class="menu-item"><p><a href="">menu 2-1</a></p></div>
-  <div class="menu-item"><p><a href="">menu 2-2</a></p></div>
-  <div class="menu-item"><p><a href="">menu 2-3</a></p></div>
-  <div class="menu-item"><p><a href="">menu 2-4</a></p></div>
+  <div class="menu-item"><p class="nav-link">演奏会チケット情報初期化(取扱注意)</p><a href=""></a></div>
+  <div class="menu-item"><p class="nav-link">チケット総返却命令</p><a href=""></a></div>
 </div>
-<div class="menu-item"><p><a href="<?=SERVER."/views/everyone/index.php"?>">団員用ページへ</a></p></div>
-<div class="menu-item"><p><a href="<?=SERVER."/views/private/index.php"?>">渉外用ページへ</a></p></div>
+<div class="menu-item"><p class="nav-link">渉外用ページへ</p><a href="<?=SERVER."/views/private/index.php"?>"></a></div>
+
+<div class="menu-item"><p class="nav-link">団員用ページへ</p><a href="<?=SERVER."/views/everyone/index.php"?>"></a></div>
