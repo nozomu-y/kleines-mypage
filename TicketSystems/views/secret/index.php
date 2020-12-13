833d629e1199d8308c29c0e8a7483bdfeaab4431
yakou1000 <yakou10004129@gmail.com>
2020-12-13 23:29:56 +0900

    create files

---
+++ TicketSystems/views/secret/index.php
@@ -0,0 +1,10 @@
+<?php
+  require_once($_SERVER['DOCUMENT_ROOT'].'/TicketSystems/kleines-mypage/TicketSystems/config/config.php');
+  $pageTitle = "団員用TOP";
+  $applyStyle = "secret";
+  require_once(ROOT.'/include/header.php');
+?>
+<p class="tx">top page test</p>
+<h2>Sub Title</h2>
+<p class="tx">sub title</p>
+<?php require_once(ROOT.'/include/footer.php'); ?>
