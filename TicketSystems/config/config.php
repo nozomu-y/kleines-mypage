70f20e5a8fc50c506899eafaca4bec078a0782be
yakou1000 <yakou10004129@gmail.com>
2020-12-13 23:11:36 +0900

    edit config

--- TicketSystems/config/config.php
+++ TicketSystems/config/config.php
@@ -6,8 +6,8 @@ if(strcmp(MODE,'test')==0){
   define('DB_HOST','localhost');
   define('DB_USER','yakou1000');
   define('DB_PASS','YakouPhpDev');
-  define('ROOT',$_SERVER['DOCUMENT_ROOT'].'/TicketSystems/mypage/TicketSystems');
-  define('SERVER','//'.$_SERVER['SERVER_NAME'].'/TicketSystems/mypage/TicketSystems');
+  define('ROOT',$_SERVER['DOCUMENT_ROOT'].'/TicketSystems/kleines-mypage/TicketSystems');
+  define('SERVER','//'.$_SERVER['SERVER_NAME'].'/TicketSystems/kleines-mypage/TicketSystems');
 }else if(strcmp(MODE,"real")==0){
   require_once('/home/chorkleines/www/member/mypage/Core/config.php');
   define('ROOT','/home/chorkleines/www/member/mypage/TicketSystems');
