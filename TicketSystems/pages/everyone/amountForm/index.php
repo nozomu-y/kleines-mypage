425139061c4251f9cb52f503a8b275c1b2d7a3a9
yakou1000 <yakou10004129@gmail.com>
2020-12-21 18:23:13 +0900

    update amountForm

--- TicketSystems/pages/everyone/amountForm/index.php
+++ TicketSystems/pages/everyone/amountForm/index.php
@@ -6,6 +6,47 @@
   $applyStyle = "everyone";
   require_once(ROOT.'/include/header.php');
 ?>
-<p class="tx">改装中</p>
-<p class="tx">指定フォーム：<?=$orderType?></p>
+<p class="tx">
+  改装中<br>
+  指定フォーム：<?=$orderType?>
+</p>
+<p class="tx"><?=$message?></p>
+<form method="post" action="#" class="needs-validation" novalidate>
+  <div class="form-group">
+    <input type="text" class="form-text" name="amount" id="amount" placeholder="枚数を入力してください" required>
+    <div class="invalid-feedback"><?php //枚数を入力してください ?></div>
+  </div>
+  <div class="form-group">
+    <?php /*<input type="hidden" name="personID" value="<?=h($_SESSION['mypage_personID'])?>" */ ?>
+    <input type="hidden" name="orderTypeID" value="<?=$orderTypeID?>">
+  </div>
+  <button class="btn btn-primary js-modal-open" data-target="confirmModal">入力確認</button>
+  <div class="modal js-modal" id="confirmModal">
+    <div class="modal-bg js-modal-close"></div>
+    <div class="modal-content">
+      <div class="modal-header"><div class="modal-title">入力確認</div>
+        <span class="modal-cross js-modal-close"><span class="cross1"></span><span class="cross2"></span></span>
+      </div>
+      <div class="modal-main">
+        <p class="tx">
+          この内容で送信してもよろしいですか？<br>
+          名前：???<br>
+          枚数：???<br>
+          オーダー種別：<?=$orderType?>
+        </p>
+      </div>
+      <div class="modal-footer">
+        <div class="modal-left">
+          <button class="btn btn-secondary">戻る</button>
+        </div>
+        <div class="modal-right">
+          <button class="btn btn-primary">送信</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</form>
+<!-- validationのjsをおく-->
+
+<script src="<?=SERVER?>/pages/js/modal.js"></script>
 <?php require_once(ROOT.'/include/footer.php'); ?>
--- TicketSystems/pages/everyone/amountForm/ticketTypeController.php
+++ TicketSystems/pages/everyone/amountForm/ticketTypeController.php
@@ -2,15 +2,19 @@
   //GETメソッドでIDを取得
   //許可されたIDの場合、それ用の操作を行う
   //IDごとにpageType用の変数を振り分ける
-  if($_GET['orderType'] == 1){
+  $orderTypeID = $_GET['orderTypeID'];
+  if($orderTypeID == 1){
     $orderType = 'request';
     $pageTitle_ = 'チケット希望フォーム';
-  }else if($_GET['orderType']==3){
+    $message = '欲しい枚数を入力してください';
+  }else if($orderTypeID == 3){
     $orderType = 'want_return';
     $pageTitle_ = "チケット返却希望フォーム";
-  }else if($_GET['orderType']==6){
+    $message = '返却する枚数を入力してください';
+  }else if($orderTypeID == 6){
     $orderType = 'cancel';
     $pageTitle_ = "チケットキャンセル報告";
+    $message = "返品された枚数を入力してください";
   }else{
     //不正な操作が行われた時、それ相応の対応をする
     $_SESSION['tp_status'] = "invalidPage";
