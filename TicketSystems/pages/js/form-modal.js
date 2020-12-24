cbbec8d989a88474e895f864aa2938711c4a584f
yakou1000 <yakou10004129@gmail.com>
2020-12-24 14:41:00 +0900

    modify form-modal.js

--- TicketSystems/pages/js/form-modal.js
+++ TicketSystems/pages/js/form-modal.js
@@ -7,7 +7,7 @@
  */
 
  $(function(){
-  var forms = document.getElementsByClassName('needs-validation');  //forms:HTMLコレクション。配列と似ている
+  var forms = document.getElementsByClassName('needs-validation');
   var form = forms[0];
 
   //入力確認ボタンをクリックした時の挙動
@@ -18,28 +18,48 @@
     for(var i=0; i<items.length; i++){
       //必須項目が未入力の場合、対応するフィードバックを表示
       if(items[i].checkValidity() === false){
-        $(items[i]).nextAll('.required-feedback').css({'display':'block'});
+        show_invalid(items[i], '.required-feedback');
         valid = false;
         continue;
       }
-      $(items[i]).nextAll('.required-feedback').css({'display':'none'});
+      hide_invalid(items[i], '.required-feedback');
       //入力項目のフォーマット確認
+      var valid_item = true;  //そのアイテムのバリデーション結果
+      //特殊文字のエスケープ
+      if(items[i].value.match(/(<|>|&|"|')/)){
+        valid = false;
+        valid_item = false;
+        show_invalid(items[i], '.invalid-chars');
+        continue;
+      }else{
+        hide_invalid(items[i], '.invalid-chars');
+      }
       if(items[i].name == "name"){
-        //数字が入っていないか
+        if(items[i].value.match(/\d/)){ //数字が入っていないか
+          valid = false;
+          valid_item = false;
+        }
       }else if(items[i].name == "amount"){
-        //数値のみか
-        //TODO 正規表現に変更
         //TODO 枚数オーバーの確認
         var num = Number(items[i].value);
-        console.log(num);
-        if(!Number.isInteger(num) || num < 0){
+        if(!Number.isInteger(num) || num < 0){  //0以上の整数のみ
           valid = false;
-          $(items[i]).nextAll('.format-feedback').css({'display':'block'});
+          valid_item = false;
         }else{
           items[i].value = Number.parseInt(num);
-          $(items[i]).nextAll('.format-feedback').css({'display':'none'});
         }
-      } //TODO その他、条件ごとに整理する。メソッド化すると良い
+      }else if(items[i].name.match("kana")){
+        if(!(items[i].value.match(/^[\u30a0-\u30ff]+$|^[\u3040-\u309f]+$/))){  //全角カナorかなのみ
+          valid = false;
+          valid_item = false;
+        }
+      }
+      //TODO その他、条件ごとに整理する
+      if(!valid_item){
+        show_invalid(items[i], '.format-feedback');
+      }else{
+        hide_invalid(items[i], '.format-feedback');
+      }
     }
     if(valid){
       //モーダルの中身を取得
@@ -62,8 +82,19 @@
     return false;  //動作をストップ
   });
 
+  //モーダル出力時の閉じる動作
   $('.js-modal-close').on('click',function(){
     $('.js-modal').fadeOut();
     return false;
   });
 });
+
+function show_invalid(items, msg_class){
+  $(items).css({'box-shadow':'2px 2px #f14343, -2px -2px #f14343'});
+  $(items).nextAll(msg_class).css({'display':'block'});
+}
+
+function hide_invalid(items, msg_class){
+  $(items).css({'box-shadow':'none'});
+  $(items).nextAll(msg_class).css({'display':'none'});
+}
