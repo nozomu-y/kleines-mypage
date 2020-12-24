f9a33009aaca626066d4f589fe585b51b6d5cd03
yakou1000 <yakou10004129@gmail.com>
2020-12-24 14:59:26 +0900

    create initialSettings

--- TicketSystems/include/navbar/secret.php
+++ TicketSystems/include/navbar/secret.php
@@ -1,7 +1,7 @@
 <div class="menu-block">
   <input type="checkbox" id="menu-bar01" class="menu-bar">
   <label for="menu-bar01">渉外チーフ用メニュー</label>
-  <div class="menu-item"><p class="nav-link">演奏会チケット情報初期化(取扱注意)</p><a href=""></a></div>
+  <div class="menu-item"><p class="nav-link">演奏会チケット情報初期化(取扱注意)</p><a href="<?=SERVER?>/pages/secret/initialSettings/index.php"></a></div>
   <div class="menu-item"><p class="nav-link">チケット総返却命令</p><a href=""></a></div>
 </div>
 <div class="menu-spacer"></div>
---
+++ TicketSystems/pages/secret/initialSettings/index.php
@@ -0,0 +1,10 @@
+<?php
+  //require_once($_SERVER['DOCUMENT_ROOT']."/TicketSystems/kleines-mypage/Common/init_page.php");
+  require_once($_SERVER['DOCUMENT_ROOT'].'/TicketSystems/kleines-mypage/TicketSystems/config/config.php');
+  $pageTitle = "初期化";
+  $applyStyle = "secret";
+  require_once(ROOT.'/include/header.php');
+?>
+<p class="tx">初期化しますか？</p>
+
+<?php require_once(ROOT.'/include/footer.php'); ?>
