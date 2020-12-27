fede977f97b37063df5f44119a7388d9c7d8a27a
yakou1000 <yakou10004129@gmail.com>
2020-12-28 01:09:13 +0900

    Remove comments

--- TicketSystems/pages/js/form-block-removable.js
+++ TicketSystems/pages/js/form-block-removable.js
@@ -4,12 +4,9 @@ window.addEventListener('DOMContentLoaded',function(){
   //既に存在するform-blockの最大ID
   var max_form_index = $(".form-block").length - 1;
 
-  //memo 戻ってきたときはこの変数の値を変えたい
-
   $('button.js-fb-add').on('click',addFormBlock);  //addボタンがクリックされた時の挙動
   $('button.js-fb-remove').on('click',removeFormBlock);  //removeボタンがクリックされた時の挙動
 
-  //読み込み時に、POSTに配列があったらそれの回数分addReserveを呼び出す？
   function addFormBlock(){
     //form-blockの内容をコピーし、次の部分に表示する
     var $original = $('#form-block\\[' + max_form_index + '\\]'); //最後のブロックを選択
@@ -26,8 +23,7 @@ window.addEventListener('DOMContentLoaded',function(){
         });
         $(obj).val('');
       });
-    // clone取得
-    var clone = $('#form-block\\[' + max_form_index + '\\]');
+    var clone = $('#form-block\\[' + max_form_index + '\\]'); // clone取得
     //removableにし、削除ボタンをshow
     $(clone).addClass('js-fb-removable');
     clone.find('.js-fb-remove').show();
@@ -35,9 +31,6 @@ window.addEventListener('DOMContentLoaded',function(){
   };
   
   function removeFormBlock(){
-    //自分のid番号を取得
-    //自分のid番号をもつ親要素のidを取得
-    //hideしてremove
     var $removeObj = $(this).parents('.js-fb-removable');
     //$removeObj.hide('fast', function() {  //fadeout後にfunctionを呼び出し
     $removeObj.remove();
@@ -50,7 +43,6 @@ window.addEventListener('DOMContentLoaded',function(){
           .attr('id', 'form-block\[' + max_form_index + '\]') // id属性を変更
           .find('input, textarea').each(function(idx, obj) {
           $(obj).attr({
-            //id: $(obj).attr('id').replace(/\[[0-9]\]+$/, '[' + max_form_index + ']'),
             name: $(obj).attr('name').replace(/\[[0-9]\]+$/, '[' + max_form_index + ']')
           });
         });
