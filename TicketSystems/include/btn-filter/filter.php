eaf971ebdd938a4efd62bae592bec940ce6a0fc0
yakou1000 <yakou10004129@gmail.com>
2020-12-30 17:48:27 +0900

    Add filter.php

---
+++ TicketSystems/include/btn-filter/filter.php
@@ -0,0 +1,36 @@
+<?php
+  require_once($_SERVER['DOCUMENT_ROOT'].'/TicketSystems/kleines-mypage/Common/dbconnect.php');
+  $parts = ["S", "A", "T", "B"];
+  $GRADE_NUM = 5; //4学年+上級生として5つにまとめる
+  $q_select = "SELECT DISTINCT(grade) FROM members ORDER BY grade DESC LIMIT $GRADE_NUM";  //上級生をまとめるために逆順で5つ取得
+  $result = $mysqli->query($q_select);
+  if($result==NULL){
+    //error
+  }
+  $grades_rev = [];
+  while($row = $result->fetch_array(MYSQLI_ASSOC)){
+    $grades_rev[] = $row['grade'];
+  }
+  $result->free();
+  $grades_rev[$GRADE_NUM-1] .= "以上";  //"(5年生の入学年度)以上"とし、上級生をまとめる
+  $grades = array_reverse($grades_rev);
+?>
+<div class="form-filter js-filter-part">
+  <p class="tx" style="font-size:18px;">学年</p>
+  <button class="btn btn-secondary" name="filter-part-clear" style="display:inline;">Clear</button>
+  <button class="btn btn-secondary" name="filter-part-clear" style="display:inline;">All</button><br>
+  <?php foreach($parts as $part): ?>
+  <input class="filter-checkbox" type="checkbox" name="filter-part[]" id="filter-<?=$part?>" value="<?=$part?>" autocomplete="off" style="display:none;" checked>
+  <label for="filter-<?=$part?>" class="btn btn-filter"><?=$part?></label>
+  <?php endforeach; ?>
+</div>
+
+<div class="form-filter js-filter-grade">
+<p class="tx" style="font-size:18px;">パート</p>
+  <button class="btn btn-secondary" name="filter-grade-clear" style="display:inline;">Clear</button>
+  <button class="btn btn-secondary" name="filter-part-clear" style="display:inline;">All</button><br>
+  <?php foreach($grades as $grade): ?>
+  <input class="filter-checkbox" type="checkbox" name="filter-part[]" id="filter-<?=$grade?>" value="<?=$grade?>" autocomplete="off" style="display:none;" checked>
+  <label for="filter-<?=$grade?>" class="btn btn-filter"><?=$grade?></label>
+  <?php endforeach; ?>
+</div>
