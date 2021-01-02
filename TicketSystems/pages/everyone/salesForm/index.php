431f6fe062dea5268fb019dd2d4855f8e29a7f60
yakou1000 <yakou10004129@gmail.com>
2021-01-02 13:12:11 +0900

    Add some index.php files

---
+++ TicketSystems/pages/everyone/salesForm/index.php
@@ -0,0 +1,9 @@
+<?php
+  //require_once($_SERVER['DOCUMENT_ROOT']."/TicketSystems/kleines-mypage/Common/init_page.php");
+  require_once($_SERVER['DOCUMENT_ROOT'].'/TicketSystems/kleines-mypage/TicketSystems/config/config.php');
+  $pageTitle = "チケット販売報告フォーム";
+  $applyStyle = "everyone";
+  require_once(ROOT.'/include/header.php');
+?>
+<p class="tx">改装中</p>
+<?php require_once(ROOT.'/include/footer.php'); ?>
---
+++ TicketSystems/pages/everyone/soldSeatList/index.php
@@ -0,0 +1,9 @@
+<?php
+  //require_once($_SERVER['DOCUMENT_ROOT']."/TicketSystems/kleines-mypage/Common/init_page.php");
+  require_once($_SERVER['DOCUMENT_ROOT'].'/TicketSystems/kleines-mypage/TicketSystems/config/config.php');
+  $pageTitle = "販売済座席一覧";
+  $applyStyle = "everyone";
+  require_once(ROOT.'/include/header.php');
+?>
+<p class="tx">改装中</p>
+<?php require_once(ROOT.'/include/footer.php'); ?>
