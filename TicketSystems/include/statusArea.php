46d6fb84bf80d6780d6a730ad1fa7c67059a6083
yakou1000 <yakou10004129@gmail.com>
2021-01-03 00:04:03 +0900

    Update statusArea

--- TicketSystems/include/statusArea.php
+++ TicketSystems/include/statusArea.php
@@ -4,21 +4,33 @@
    */
   function printStatusMessage($key){
     if(strcmp($key,"complete_init")==0){
-      echo("初期化が完了しました\n");
+      echo("初期化が完了しました");
+    }else if(strcmp($key,"succeed_signin")==0){
+      echo("サインインに成功しました");
+    }else if(strcmp($key,"wrong-email")==0){
+      echo("存在しないメールアドレスです");
+    }else if(strcmp($key,"wrong-password")==0){
+      echo("パスワードが違います");
     }else{
-      echo($key);
+      echo("statusArea.phpで存在しないkey:\n"+$key);
     }
   }
 ?>
 
 <div class="status-area">
-  <?php
-    if(isset($_SESSION['tp_status'])){
-      printStatusMessage($_SESSION['tp_status']);
-      unset($_SESSION['tp_status']);
-    }
-    if(isset($_POST['tp_status'])){
-      printStatusMessage($_POST['tp_status']);
-    }
-  ?>
+  <p class="tx">
+    <?php
+      if(isset($_SESSION['tp_status'])){
+        printStatusMessage($_SESSION['tp_status']);
+        unset($_SESSION['tp_status']);
+      }
+      if(isset($_SESSION['tp_error'])){
+        printStatusMessage($_SESSION['tp_error']);
+        unset($_SESSION['tp_error']);
+      }
+      if(isset($_POST['tp_status'])){
+        printStatusMessage($_POST['tp_status']);
+      }
+    ?>
+  </p>
 </div>
