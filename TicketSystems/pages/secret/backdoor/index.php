4af76086f99f3e0d91a35eb16b9294b5a081d22f
yakou1000 <yakou10004129@gmail.com>
2021-01-03 17:45:08 +0900

    Fix typo

--- TicketSystems/pages/secret/backdoor/index.php
+++ TicketSystems/pages/secret/backdoor/index.php
@@ -5,7 +5,7 @@
   //パスワードの確認
   if(isset($_POST['process']) && strcmp($_POST['process'], "secret-pass")==0){
     require_once __DIR__ . "/checkPassword.php";
-    header("Location: ".$_SESSION['PHP_SELF']);
+    header("Location: ".$_SERVER['PHP_SELF']);
     exit();
   }
 
@@ -14,7 +14,7 @@
   require_once TP_ROOT.'/include/header.php';
 ?>
 <p class="tx">初期設定のためのパスワードを入力してください。</p>
-<form class="needs-validation" action="<?=$_SESSION['PHP_SELF']?>" method="post">
+<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
   <input class="form-text" type="password" name="password" required>
   <input type="hidden" name="process" value="secret-pass">
   <button class="btn btn-primary" type="submit">送信</button>
