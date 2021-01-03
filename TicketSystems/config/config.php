9d4b513e11b5bd701d149a7e37ce42c83bab8d7f
yakou1000 <yakou10004129@gmail.com>
2021-01-03 16:59:53 +0900

    Add secret password to config

--- TicketSystems/config/config.php
+++ TicketSystems/config/config.php
@@ -1,13 +1,15 @@
 <?php
-  define('MODE','test');
+  define('MODE', 'test');
 
-  define('SITE_NAME','CK_TicketSystem');
+  define('SITE_NAME', 'CK_TicketSystem');
+  define('NO_PERM_NUM', 999);
+  define('SECRET_PASS', '09yampapa26');
 if(strcmp(MODE,'test')==0){
-  define('TP_ROOT',$_SERVER['DOCUMENT_ROOT'].'/TicketSystems/kleines-mypage/TicketSystems');
-  define('TP_SERVER','//'.$_SERVER['SERVER_NAME'].'/TicketSystems/kleines-mypage/TicketSystems');
+  define('TP_ROOT', $_SERVER['DOCUMENT_ROOT'].'/TicketSystems/kleines-mypage/TicketSystems');
+  define('TP_SERVER', '//'.$_SERVER['SERVER_NAME'].'/TicketSystems/kleines-mypage/TicketSystems');
   
-}else if(strcmp(MODE,"real")==0){
-  define('TP_ROOT', WEB_DOMAIN . MYPAGE_ROOT . "/TicketSystems");
+}else if(strcmp(MODE, "real")==0){
+  define('TP_ROOT', '//' . WEB_DOMAIN . MYPAGE_ROOT . "/TicketSystems");
   define('TP_SERVER', TP_ROOT);
 }
 
