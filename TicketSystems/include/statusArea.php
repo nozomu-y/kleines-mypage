06e69d85194b80258b458dafcfdca4a60a1d7911
yakou1000 <yakou10004129@gmail.com>
2021-01-05 21:30:44 +0900

    Use switch instead of elseif

--- TicketSystems/include/statusArea.php
+++ TicketSystems/include/statusArea.php
@@ -3,28 +3,40 @@
    * tp_statusの文字列を表示する文字列に変換する関数
    */
   function printStatusMessage($key){
-    if(strcmp($key,"complete_init")==0){
-      echo("初期化が完了しました");
-    }else if(strcmp($key,"succeed_signin")==0){
-      echo("サインインに成功しました");
-    }else if(strcmp($key,"wrong-email")==0){
-      echo("存在しないメールアドレスです");
-    }else if(strcmp($key,"wrong-password")==0){
-      echo("パスワードが違います");
-    }else if(strcmp($key,"not-permittied")==0){
-      echo("アクセスする権限がありません");
-    }else if(strcmp($key,"invalid-page")==0){
-      echo("不正な操作が行われました");
-    }else if(strcmp($key,"succeed-secret")==0){
-      echo("パスワードによる認証に成功しました。<br>続いて初期化を行ってください。");
-    }else if(strcmp($key,"edit-perm")==0){
-      echo("権限の変更を適用しました");
-    }else if(strcmp($key,"complete-perm-edit")==0){
-      echo("権限の設定が完了しました");
-    }else if(strcmp($key,"succeed-submit")==0){
-      echo("送信が完了しました");
-    }else{
+    switch($key){
+      case "complete_init":
+        echo("初期化が完了しました");
+        break;
+      case "succeed_signin":
+        echo("サインインに成功しました");
+        break;
+      case "wrong-email":
+        echo("存在しないメールアドレスです");
+        break;
+      case "wrong-password":
+        echo("パスワードが違います");
+        break;
+      case "not-permittied":
+        echo("アクセスする権限がありません");
+        break;
+      case "invalid-page":
+        echo("不正な操作が行われました");
+        break;
+      case "succeed-secret":
+        echo("パスワードによる認証に成功しました。<br>続いて初期化を行ってください。");
+        break;
+      case "edit-perm":
+        echo("権限の変更を適用しました");
+        break;
+      case "complete-perm-edit":
+        echo("権限の設定が完了しました");
+        break;
+      case "succeed-submit":
+        echo("送信が完了しました");
+        break;
+      default:
       echo("statusArea.phpで存在しないkey:<br>"+$key);
+      break;
     }
   }
 ?>
--- TicketSystems/pages/everyone/amountForm/ticketTypeController.php
+++ TicketSystems/pages/everyone/amountForm/ticketTypeController.php
@@ -1,23 +1,24 @@
 <?php
-  //GETメソッドでIDを取得
-  //許可されたIDの場合、それ用の操作を行う
-  //IDごとにpageType用の変数を振り分ける
   $orderTypeID = $_GET['orderTypeID'];
-  if($orderTypeID == 1){
-    $orderType = 'request';
-    $pageTitle_ = 'チケット希望フォーム';
-    $message = '欲しい枚数を入力してください';
-  }else if($orderTypeID == 3){
-    $orderType = 'want_return';
-    $pageTitle_ = "チケット返却希望フォーム";
-    $message = '返却する枚数を入力してください';
-  }else if($orderTypeID == 6){
-    $orderType = 'cancel';
-    $pageTitle_ = "チケットキャンセル報告";
-    $message = "返品された枚数を入力してください";
-  }else{
-    //不正な操作が行われた時、それ相応の対応をする
-    $_SESSION['tp_status'] = "invalid-page";
-    header("Location: ../index.php");
-    exit();
+  switch($orderTypeID){
+    case 1:
+      $orderType = 'request';
+      $pageTitle_ = 'チケット希望フォーム';
+      $message = '欲しい枚数を入力してください';
+      break;
+    case 3:
+      $orderType = 'want_return';
+      $pageTitle_ = "チケット返却希望フォーム";
+      $message = '返却する枚数を入力してください';
+      break;
+    case 6:
+      $orderType = 'cancel';
+      $pageTitle_ = "チケットキャンセル報告";
+      $message = "返品された枚数を入力してください";
+      break;
+    default:
+      //不正な操作が行われた時、TOPに飛ばす
+      $_SESSION['tp_status'] = "invalid-page";
+      header("Location: ../index.php");
+      exit();
   }
