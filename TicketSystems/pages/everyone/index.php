41a5f384f91d2c49d3e3c69ed7fe5442ea6e7431
yakou1000 <yakou10004129@gmail.com>
2021-01-06 14:05:02 +0900

    Add description

--- TicketSystems/pages/everyone/index.php
+++ TicketSystems/pages/everyone/index.php
@@ -5,7 +5,18 @@
   $applyStyle = "everyone";
   require_once TP_ROOT.'/include/header.php';
 ?>
-<p class="tx">top page test</p>
-<h2>Sub Title</h2>
-<p class="tx">sub title</p>
+<p class="tx">チケットの申し込み・各種手続きを行うサイトです</p>
+<h2>使用時の注意</h2>
+<p class="tx">
+  JavaScriptを有効にしてください！<br>
+  さもないと正常に動作しません。<br>
+  有効にする方法を載せたい<br>
+  JavaScriptが有効になっているか確認↓<br>
+  <a class="btn btn-primary" href="#">確認</a>
+  <br>↑クリックしてリンク先で遊べた人は有効！みたいな
+</p>
+<h2>渉外からのお知らせ</h2>
+<p class="tx">ニュース形式で表示できたりしたらいいよね</p>
+<h2>CK_Ticketの使い方</h2>
+<p class="tx">ケース別で紹介するとか動画・マニュアル作るとか</p>
 <?php require_once TP_ROOT.'/include/footer.php'; ?>
