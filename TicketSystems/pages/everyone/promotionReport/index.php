1326ae9aea638b4268f0ff157b0634a999010056
yakou1000 <yakou10004129@gmail.com>
2021-01-06 16:37:02 +0900

    Delete unnecessary file

--- TicketSystems/pages/everyone/promotionReport/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once __DIR__.'/../../../include/tp_init.php';
-  saccessFilter(NO_PERM_NUM, $USER->id, $mysqli);
-  $pageTitle = "情宣完了報告";
-  $applyStyle = "everyone";
-  require_once TP_ROOT.'/include/header.php';
-?>
-<p class="tx">改装中</p>
-<?php require_once TP_ROOT.'/include/footer.php'; ?>
