4a9386b1fbe22dcb8c69965d0bf8e2ef709c761d
nozoyama39 <nozoyama39@gmail.com>
2021-03-01 20:07:48 +0900

    Update: info

--- info/index.php
+++ info/index.php
@@ -2,24 +2,57 @@
 require __DIR__ . '/../Common/init_page.php';
 include_once __DIR__ . '/../Common/head.php';
 ?>
-<div class="container-fluid">
-    <div class="row justify-content-xl-center">
-        <div class="col-xl-6 mb-3">
-            <div id="readme-md">
-                <div class="alert alert-danger">
-                    Error: Unable to load markdown file.
+
+<div class="d-none d-md-block">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-6 mb-3">
+                <div class="card mb-3">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">README.md</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="markdown-body" id="readme-md">
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div id="changelog-md">
-                <div class="alert alert-danger">
-                    Error: Unable to load markdown file.
+            <div class="col-xl-6 mb-3">
+                <div class="card mb-3">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">CHANGELOG.md</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="markdown-body" id="changelog-md">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
 </div>
+
+<div class="d-block d-md-none">
+    <div class="card card-flush mb-3">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">README.md</h6>
+        </div>
+        <div class="card-body">
+            <div class="markdown-body" id="readme-md-sm">
+            </div>
+        </div>
+    </div>
+    <div class="card card-flush mb-3">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">CHANGELOG.md</h6>
+        </div>
+        <div class="card-body">
+            <div class="markdown-body" id="changelog-md-sm">
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php
 $script .= '<script src="' . MYPAGE_ROOT . '/Resources/js/marked.min.js' . '"></script>';
 
@@ -35,12 +68,20 @@ $script .= 'var link = document.getElementById("readme-md").getElementsByTagName
         for (var i = 0, len = link.length; i < len; ++i) {
             link[i].setAttribute("target", "_blank");
         }';
-$script .= 'document.getElementById("installation").remove();';
-$script .= 'document.getElementById("changelog").remove();';
-$script .= 'var h2 = document.getElementById("readme-md").getElementsByTagName("h2");';
-$script .= 'for (var i = 0, len = h2.length; i < len; ++i) {
-        h2[i].after(document.createElement("hr"));
+$script .= 'document.getElementById("readme-md").getElementById("installation").remove();';
+$script .= 'document.getElementById("readme-md").getElementById("changelog").remove();';
+$script .= '</script>';
+
+$script .= '<script>';
+$script .= 'var markdown = "' . $readme_md . '";';
+$script .= 'var html = marked(markdown);$("#readme-md-sm").html(html);';
+$script .= 'document.getElementById("readme-md-sm").getElementsByTagName("h1")[0].innerHTML = "<a href=\"https://github.com/nozomu-y/kleines-mypage\">Kleines Mypage</a>";';
+$script .= 'var link = document.getElementById("readme-md-sm").getElementsByTagName("a");
+        for (var i = 0, len = link.length; i < len; ++i) {
+            link[i].setAttribute("target", "_blank");
         }';
+$script .= 'document.getElementById("readme-md-sm").getElementById("installation").remove();';
+$script .= 'document.getElementById("readme-md-sm").getElementById("changelog").remove();';
 $script .= '</script>';
 
 $changelog_md = file_get_contents("../CHANGELOG.md");
@@ -60,9 +101,22 @@ $script .= '
                 return "<h"+String(i+1)+">" + $(this).html() + "</h"+String(i+1)+">";
             });
         }';
-$script .= 'var h2 = document.getElementById("changelog-md").getElementsByTagName("h2");';
-$script .= 'for (var i = 0, len = h2.length; i < len; ++i) {
-        h2[i].after(document.createElement("hr"));
+$script .= 'document.getElementById("changelog-md").getElementsByTagName("h2")[0].remove();';
+$script .= '</script>';
+
+$script .= '<script>';
+$script .= 'var markdown = "' . $changelog_md . '";';
+$script .= 'var html = marked(markdown);$("#changelog-md-sm").html(html);';
+$script .= 'var link = document.getElementById("changelog-md-sm").getElementsByTagName("a");
+        for (var i = 0, len = link.length; i < len; ++i) {
+            link[i].setAttribute("target", "_blank");
         }';
+$script .= '
+        for (var i = 4; i >= 1; i--) {
+            $("#changelog-md-sm h"+String(i)).replaceWith(function() {
+                return "<h"+String(i+1)+">" + $(this).html() + "</h"+String(i+1)+">";
+            });
+        }';
+$script .= 'document.getElementById("changelog-md-sm").getElementsByTagName("h2")[0].remove();';
 $script .= '</script>';
 include_once __DIR__ . '/../Common/foot.php';
